6c0f49b6d1eb6f1b2fae8ba04896c25fe0c57cf1
Sherathiya Deep <sherathiyadeep2469@gmail.com>
2021-07-08 18:03:31 +0530

    Add files via upload

---
+++ Eclipse-workspace-collection/JDBC/src/com/phoenix/dbconfig/OracleConnection.java
@@ -0,0 +1,17 @@
+package com.phoenix.dbconfig;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class OracleConnection {
+	
+	public static Connection getOracleConnection() throws ClassNotFoundException, SQLException {
+		
+		String jdbcDriver = "oracle.jdbc.driver.OracleDriver";
+		String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:XE";
+		String dbUser = "hr";
+		String dbPassword = "hr";
+		
+		return DatabaseConfig.getConnection(jdbcDriver, jdbcUrl, dbUser, dbPassword);
+	}
+}
